e918c247788dd5b0dbe2488f673a616256735969
ascetik <ascetik@free.fr>
2023-11-05 12:16:39 +0100

    Cacheable interface lost run and callable methods

--- src/Types/Cacheable.php
+++ src/Types/Cacheable.php
@@ -23,8 +23,8 @@ namespace Ascetik\Cacheable\Types;
  */
 interface Cacheable
 {
-    public function run(array $parameters = []): mixed;
-    public function callable(): callable;
+    // public function run(array $parameters = []): mixed;
+    // public function callable(): callable;
     public function encode():string;
     public function decode(string $serial):void;
 }
