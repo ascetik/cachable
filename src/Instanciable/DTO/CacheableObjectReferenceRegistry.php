3aba794270cc6e58f7ab5760277b1850fb1af749
ascetik <ascetik@free.fr>
2023-11-06 19:08:05 +0100

    trying to figure out needed methods for registry

--- src/Instanciable/DTO/CacheableObjectReferenceRegistry.php
+++ src/Instanciable/DTO/CacheableObjectReferenceRegistry.php
@@ -4,7 +4,47 @@ declare(strict_types=1);
 
 namespace Ascetik\Cacheable\Instanciable\DTO;
 
-class CacheableObjectReferenceRegistry
+use Ascetik\Cacheable\Types\Cacheable;
+use Ascetik\Cacheable\Types\CacheableObjectReference;
+use Ds\Set;
+
+class CacheableObjectReferenceRegistry implements Cacheable
 {
-    //
+    /**
+     * Reference container
+     *
+     * @var Set<CacheableObjectReference>
+     */
+    private Set $container;
+    public function __construct(iterable $content = [])
+    {
+        $this->container = new Set($content);
+    }
+
+    public function assign(int $amount):void
+    {
+        $this->container->allocate($amount);
+    }
+
+    public function list():Set
+    {
+        return $this->container->copy();
+    }
+
+    public function push(CacheableObjectReference $reference)
+    {
+        $this->container->add($reference);
+    }
+
+    public function serialize(): string
+    {
+        return serialize($this->container->toArray());
+    }
+
+    public function unserialize(string $data): void
+    {
+        /** @var CacheableObjectReference[] $references */
+        $references = unserialize($data);
+        $this->container = new Set($references);
+    }
 }
