6ae52ec43c1c841b584afb042b5133a15d32f1e2
ascetik <ascetik@free.fr>
2023-11-06 19:04:10 +0100

    CacheableObjectReference doc

--- src/Types/CacheableObjectReference.php
+++ src/Types/CacheableObjectReference.php
@@ -1,8 +1,27 @@
 <?php
 
+/**
+ * This is part of the ascetik/cacheable package
+ *
+ * @package    Cacheable
+ * @category   interface Type
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+ declare(strict_types=1);
+
 namespace Ascetik\Cacheable\Types;
 
+/**
+ * Implemented by object reference classes
+ * such as CacheableProperty or CacheableInstance
+ *
+ * @version 1.0.0
+ */
 interface CacheableObjectReference extends Cacheable
 {
-    //
+    public function getName(): string;
+    public function getValue(): mixed;
 }
